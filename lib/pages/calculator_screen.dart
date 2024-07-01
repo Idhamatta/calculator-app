01469620ec668687fa8462e35d5875a6e134ab42
Idham Atta <idhamatta2706@gmail.com>
2024-07-01 22:23:23 +0700

    completed the logic button

--- lib/pages/calculator_screen.dart
+++ lib/pages/calculator_screen.dart
@@ -98,7 +98,72 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
       clearAll();
       return;
     }
+
+    if (value == Btn.per) {
+      convertToPercentage();
+    }
+
+    if (value == Btn.calculate) {
+      calculate();
+      return;
+    }
+
     appendValue(value);
+  }
+
+  //
+  void calculate() {
+    if (number1.isEmpty) return;
+    if (operand.isEmpty) return;
+    if (number2.isEmpty) return;
+
+    double num1 = double.parse(number1);
+    double num2 = double.parse(number2);
+
+    var result = 0.0;
+    switch (operand) {
+      case Btn.add:
+        result = num1 + num2;
+        break;
+      case Btn.subtract:
+        result = num1 - num2;
+        break;
+      case Btn.multiply:
+        result = num1 * num2;
+        break;
+      case Btn.divide:
+        result = num1 / num2;
+        break;
+      default:
+    }
+    setState(() {
+      number1 = '$result';
+
+      if (number1.endsWith('.0')) {
+        number1 = number1.substring(0, number1.length - 2);
+      }
+
+      operand = '';
+      number2 = '';
+    });
+  }
+
+  void convertToPercentage() {
+    // ex: 434+324
+    if (number1.isNotEmpty && operand.isNotEmpty && number2.isNotEmpty) {
+      //calculate before conversion
+      // TODO
+    }
+    if (operand.isNotEmpty) {
+      return;
+    }
+
+    final number = double.parse(number1);
+    setState(() {
+      number1 = '${(number / 100)}';
+      operand = '';
+      number2 = '';
+    });
   }
 
   // clears all output
@@ -131,6 +196,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
       // operand pressed
       if (operand.isNotEmpty && number2.isNotEmpty) {
         // TODO calculate the equation before assigning new operate
+        calculate();
       }
       operand = value;
       // asign value to number1 variable
